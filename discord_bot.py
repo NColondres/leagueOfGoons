979725661a682637f43c6288761c250051b607f3
Nicolas Colondres <36716252+NColondres@users.noreply.github.com>
2021-11-28 22:25:51 -0500

    Created enroll and enrolled discord commands

--- discord_bot.py
+++ discord_bot.py
@@ -5,8 +5,6 @@ from src import league, database
 
 BOT_TOKEN = dotenv_values(".env")['LEAGUE_OF_GOONS_BOT_TOKEN']
 
-database.create_database('players_data.db')
-
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
@@ -16,15 +14,17 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def enroll(ctx, summoner_name: str):
-    await ctx.reply(f'{ctx.message.author}: {ctx.message.author.id}')
-    await ctx.send(league.get_summoner_info(summoner_name))
+    league_info = await league.get_summoner_info(summoner_name)
+    database.enroll_user(ctx.message.author, ctx.message.author.id, league_info["name"], league_info["puuid"])
+    await ctx.reply(f'{league_info["name"]} has been enrolled with {ctx.message.author}')
 
 @enroll.error
 async def enroll_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.reply('You fucked up...\nTo use the command type: !enroll <YourSummonerName>')
 
-async def test():
-    pass
+@bot.command()
+async def enrolled(ctx):
+    await ctx.send(database.get_enrolled_users())
 
 bot.run(BOT_TOKEN)
--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 from os import path
 import sqlite3
 
+PLAYERS_DATABASE = 'players_data.db'
 
 def create_database(name: str):
     if not path.isfile(f'./src/database/{name}'):
@@ -11,5 +12,22 @@ def create_database(name: str):
                     (discord_account text, discord_id text, league_account text, league_puuid text)''')
         con.commit()
         con.close()
+        print(f'Databse "{name}" has been created')
     else:
         print(f'Databse "{name}" already exist')
+
+create_database(PLAYERS_DATABASE)
+
+def enroll_user(discord_account: str, discord_id: str, league_account: str, league_puuid: str):
+    con = sqlite3.connect(f'./src/database/{PLAYERS_DATABASE}')
+    cur = con.cursor()
+    cur.execute("INSERT INTO players VALUES (:discord_account, :discord_id, :league_account, :league_puuid)", {'discord_account': str(discord_account), 'discord_id': str(discord_id), 'league_account': str(league_account), 'league_puuid': str(league_puuid)})
+    con.commit()
+    con.close()
+    return 'Player Enrolled'
+
+def get_enrolled_users():
+    con = sqlite3.connect(f'./src/database/{PLAYERS_DATABASE}')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM players")
+    return cur.fetchall()
