21a7404a61f93352141469c3756b48a6fdf6dd6d
Nicolas Colondres <36716252+NColondres@users.noreply.github.com>
2022-01-03 16:35:22 -0500

    Testing

--- discord_bot.py
+++ discord_bot.py
@@ -33,8 +33,8 @@ async def on_ready():
     server = bot.get_guild(LEAGUE_OF_GOONS_SERVER_ID)
     print(server)
     for member in server.members:
-        # if member.nick:
-        #    await member.edit(nick=None) 
+        if member.nick:
+           await member.edit(nick=None) 
         print(member.name, member.nick, member.id, member.top_role)
 
 @bot.event
