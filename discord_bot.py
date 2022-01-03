ee19b6aa67bcc82cc13b89fa01393539eff3fc48
Nicolas Colondres <36716252+NColondres@users.noreply.github.com>
2022-01-03 17:02:12 -0500

    Testing

--- discord_bot.py
+++ discord_bot.py
@@ -33,7 +33,7 @@ async def on_ready():
     server = bot.get_guild(LEAGUE_OF_GOONS_SERVER_ID)
     print(server)
     for member in server.members:
-        if member.nick and member.top_role.name != '@Admin':
+        if member.nick and (member.top_role.name != '@Admin' or member.id == server.owner.id):
            await member.edit(nick=None) 
         print(member.name, member.nick, member.id, member.top_role.name)
 
