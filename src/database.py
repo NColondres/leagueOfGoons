3f4d0243d27faa3ad6e25e655ede337e5169e22f
Nicolas Colondres <36716252+NColondres@users.noreply.github.com>
2022-01-12 20:15:34 -0500

    Grammatical correction.

--- src/database.py
+++ src/database.py
@@ -72,7 +72,7 @@ def unenroll_user(discord_account: str):
     cur.execute("DELETE FROM players WHERE discord_account=(:discord_account)", {'discord_account': str(discord_account)})
     con.commit()
     con.close()
-    return f'{discord_account} been unerolled'
+    return f'{discord_account} has been unerolled'
 
 def insert_match(match_id, player_id, kills, deaths, assists, champion, win, match_end_timestamp):
     con = sqlite3.connect(f'./src/database/{PLAYERS_DATABASE}')
