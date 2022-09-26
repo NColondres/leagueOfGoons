76ae051595b3f339a1e8fe6b020d203b91d5e601
Nicolas Colondres <36716252+NColondres@users.noreply.github.com>
2022-09-25 20:17:23 -0400

    test comment added again....

--- discord_bot.py
+++ discord_bot.py
@@ -26,7 +26,7 @@ POOP = os.getenv("POOP")
 help_command = commands.DefaultHelpCommand(
     no_category="Commands",
 )
-# add comment 3
+# add comment 4
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(
