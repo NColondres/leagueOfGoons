d3b5e9744f474253215aedc898a239b39ac3fa99
Nicolas <ncolondres@gmail.com>
2022-12-04 00:12:56 -0500

    created new database

---
+++ src/challenge_databse.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+DUEL_DATABASE = "duel_databse.db"
+
+
+def create_duel_database(name: str):
+    con = sqlite3.connect(f"./src/database/{name}")
+    cur = con.cursor()
+
+    # Create tables
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS challenges(
+                challenge_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                challenger text,
+                champion text);
+                """
+    )
+    con.commit()
+    con.close()
+
+create_duel_database(DUEL_DATABASE)
+
+con = sqlite3.connect(f"./src/database/{PLAYERS_DATABASE}")
+cur = con.cursor()
+cur.execute(
+    """
+    INSERT INTO challenges (challenger, champion)
+    VALUES (:challenger, :champion )
+    """,
+    {
+        "challenger": 'Nick_C',
+        "champion": 'Lucian'
+    }
+)
+
+print('\nCHALLENGER DATABSE IMPORTED\n')
