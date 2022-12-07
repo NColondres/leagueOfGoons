92a1208caeda9f1c121a35db7e3858d38e1dad6b
Nicolas <ncolondres@gmail.com>
2022-12-06 19:44:34 -0500

    simple get data function from database created

--- src/challenge_database.py
+++ src/challenge_database.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-DUEL_DATABASE = "challenge_database.db"
+CHALLENGE_DATABASE = "challenge_database.db"
 
 
 def create_duel_database(name: str):
@@ -11,24 +11,56 @@ def create_duel_database(name: str):
     cur.execute(
         """CREATE TABLE IF NOT EXISTS challenges(
                 challenge_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                challenger text,
-                champion text);
+                challenger_discord_id INTEGER NOT NULL,
+                acceptor_discord_id INTEGER,
+                decided_champion TEXT,
+                decided_role TEXT,
+                challenger_match_id TEXT,
+                acceptor_match_id TEXT,
+                challenge_complete INTEGER DEFAULT 0);
+                """
+    )
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS matches(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                challenge_id INTEGER
+                match_id TEXT,
+                player_id TEXT,
+                kills INTEGER,
+                pentaKills INTEGER,
+                deaths INTEGER,
+                assists INTEGER,
+                champion TEXT,
+                win INTEGER,
+                barons INTEGER,
+                dragons INTEGER,
+                turrets INTEGER,
+                inhibs INTEGER,
+                creepScore INTEGER,
+                visionScore INTEGER,
+                totalHealsOnTeammates INTEGER,
+                totalDamageShieldedOnTeammates INTEGER,
+                totalDamageDealtToChampions INTEGER,
+                match_end_timestamp INTEGER NOT NULL,
+                FOREIGN KEY (challenge_id) REFERENCES challenges(challenge_id));
                 """
     )
     con.commit()
     con.close()
 
 
-create_duel_database(DUEL_DATABASE)
+create_duel_database(CHALLENGE_DATABASE)
 
-con = sqlite3.connect(f"./src/database/{DUEL_DATABASE}")
-cur = con.cursor()
-cur.execute(
-    """
-    INSERT INTO challenges (challenger, champion)
-    VALUES (:challenger, :champion )
-    """,
-    {"challenger": "Nick_C", "champion": "Lucian"},
-)
-con.commit()
-con.close()
+async def get_challenges(challenger_discord_id: int):
+    con = sqlite3.connect(f"./src/database/{PLAYERS_DATABASE}")
+    cur = con.cursor()
+    data = cur.execute(
+        """
+        Select * FROM challenges
+        WHERE challenger_discord_id = (:challenger_discord_id)
+        """,
+        {"challenger_discord_id": str(discord_id)})
+    print(data)
+    print(data.description)
+    print(cur.fetchall())
+    
